b39d827e885cf9dedf90df6d4deff78fef1cce03
taknshno <126603921+taknshno@users.noreply.github.com>
2023-05-14 15:05:57 +0900

    add: Writing feature of student data by form

---
+++ sample.js
@@ -0,0 +1,21 @@
+new Vue({
+  el: '#app',
+  data: {
+    name: 'A山B郎',
+    course: 'Webエンジニアコース',
+    acceptancePeriod: '2019年01月期',
+    defaultLastId: 3,
+    students: [
+      { id: 1, name: '野呂浩良', course: '機械学習コース', acceptancePeriod: '2019年01月期' },
+      { id: 2, name: '富永修司', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' },
+      { id: 3, name: '斉藤一起', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' }
+    ]
+  },
+  methods: {
+    addStudent: function() {
+      new_id = this.students.slice(-1)[0].id + 1
+      new_student = {id: new_id, name: this.name, course: this.course, acceptancePeriod: this.acceptancePeriod }
+      this.students.push(new_student)
+    }
+  }
+})
